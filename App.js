97b44bd36ae6e3ccbd875774e20467a4352b9d64
LeviWachira <trojan.wachira@gmail.com>
2019-12-26 18:29:15 +0700

    recent add onrefresh screen #12

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
-import { StyleSheet} from 'react-native';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
-import Font from 'expo-font';
+import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
 
-import productsReducer from './src/store/reducers/products';
-import cartReducer from './src/store/reducers/cart';
-import OrdersReducer from './src/store/reducers/orders';
-import ShopNavigators from './src/navigation/ShopNavigators';
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
-  orders: OrdersReducer
+  orders: ordersReducer
 });
 
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
-const store = createStore(rootReducer);
-
-const fetchFont = () => {
-
+const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
@@ -28,30 +26,21 @@ const fetchFont = () => {
 };
 
 export default function App() {
-  const [fontLoaded, SetFontLoaded] = useState(false);
-
-
+  const [fontLoaded, setFontLoaded] = useState(false);
 
   if (!fontLoaded) {
-    <AppLoading
-      startAsync={fetchFont}
-      onFinish={() => SetFontLoaded(true)}
-    />
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
   }
-
   return (
     <Provider store={store}>
-      <ShopNavigators />
+      <ShopNavigator />
     </Provider>
-
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
